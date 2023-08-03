b69e935b94b225d802ac5935f7ca322ab9da1f36
Devin Dias <108897491+Devin-GPT@users.noreply.github.com>
2023-08-03 10:56:45 -0500

    tweaks to rm command

--- bash/modify_list_for_removal.sh
+++ bash/modify_list_for_removal.sh
@@ -2,7 +2,7 @@
 # Date: August 3, 2023
 
 # This script is used to modify the contents of the file named "answers.txt".
-# Specifically, it will add "rm -rf " at the beginning of each line and " &&" at the end of each line,
+# Specifically, it will add "rm " at the beginning of each line and " &&" at the end of each line,
 # except for the last line, which will not have " &&" at the end.
 
 # Check if the file "answers.txt" exists in the current directory.
@@ -17,7 +17,7 @@ cp answers.txt answers.txt.backup
 
 # Use the 'awk' command to modify the file.
 # 'NR > 1 { print last " &&" } { last = $0 }' prints all lines except the last with " &&" added at the end.
-# 'END { print "rm -rf " last }' prints the last line without " &&" at the end.
-awk 'NR > 1 { print "rm -rf " last " &&" } { last = $0 } END { print "rm -rf " last }' answers.txt > temp.txt && mv temp.txt answers.txt
+# 'END { print "rm " last }' prints the last line without " &&" at the end.
+awk 'NR > 1 { print "rm " last " &&" } { last = $0 } END { print "rm " last }' answers.txt > temp.txt && mv temp.txt answers.txt
 
 echo "File answers.txt has been modified. Original file is backed up as answers.txt.backup."s
